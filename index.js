59cacf45fe4c25104d5776e3b35574bb0ab337e5
Simon Moe <simmoe@gmail.com>
2019-10-02 14:25:46 +0200

    update max y,x

--- index.js
+++ index.js
@@ -1,130 +1,149 @@
-$(function(){
+$(function () {
 
-  var el = document.createElement('div'),
-      transformProps = 'transform WebkitTransform MozTransform OTransform msTransform'.split(' '),
-      transformProp = support(transformProps),
-      transitionDuration = 'transitionDuration WebkitTransitionDuration MozTransitionDuration OTransitionDuration msTransitionDuration'.split(' '),
-      transitionDurationProp = support(transitionDuration);
+    var el = document.createElement('div'),
+        transformProps = 'transform WebkitTransform MozTransform OTransform msTransform'.split(' '),
+        transformProp = support(transformProps),
+        transitionDuration = 'transitionDuration WebkitTransitionDuration MozTransitionDuration OTransitionDuration msTransitionDuration'.split(' '),
+        transitionDurationProp = support(transitionDuration);
 
-  function support(props) {
-      for(var i = 0, l = props.length; i < l; i++) {
-          if(typeof el.style[props[i]] !== "undefined") {
-              return props[i];
-          }
-      }
-  }
+    function support(props) {
+        for (var i = 0, l = props.length; i < l; i++) {
+            if (typeof el.style[props[i]] !== "undefined") {
+                return props[i];
+            }
+        }
+    }
 
-  var mouse = {
-          start : {}
-      },
-      touch = document.ontouchmove !== undefined,
-      viewport = {
-          x: -10,
-          y: 20,
-          el: $('.cube')[0],
-          move: function(coords) {
-              if(coords) {
-                  if(typeof coords.x === "number") this.x = coords.x;
-                  if(typeof coords.y === "number") this.y = coords.y;
-              }
+    var mouse = {
+            start: {}
+        },
+        touch = document.ontouchmove !== undefined,
 
-              this.el.style[transformProp] = "rotateX("+this.x+"deg) rotateY("+this.y+"deg)";
-          },
-          reset: function() {
-              this.move({x: 0, y: 0});
-          }
-      };
+        //viewport object variable
+        viewport = {
+            x: -10,
+            y: 20,
+            el: $('.cube')[0],
 
-  viewport.duration = function() {
-      var d = touch ? 50 : 2000;
-      viewport.el.style[transitionDurationProp] = d + "ms";
-      return d;
-  }();
+            //central move function
+            move: function (coords) {
+                if (coords) {
+                    if (typeof coords.x === "number") this.x = coords.x;
+                    if (typeof coords.y === "number") this.y = coords.y;
+                }
 
-  $(document).keydown(function(evt) {
-      switch(evt.keyCode)
-      {
-          case 37: // left
-              viewport.move({y: viewport.y - 90});
-              break;
+                this.x = this.x > 90 ? 90 : this.x;
+                this.x = this.x < -90 ? -90 : this.x;
+                this.y = this.y > 180 ? 180 : this.y;
+                this.y = this.y < -180 ? -180 : this.y;
 
-          case 38: // up
-              evt.preventDefault();
-              viewport.move({x: viewport.x + 90});
-              break;
+                this.el.style[transformProp] = "rotateX(" + this.x + "deg) rotateY(" + this.y + "deg)";
+            },
+            reset: function () {
+                this.move({
+                    x: 0,
+                    y: 0
+                });
+            }
+        };
 
-          case 39: // right
-              viewport.move({y: viewport.y + 90});
-              break;
+    viewport.duration = function () {
+        var d = touch ? 50 : 0;
+        viewport.el.style[transitionDurationProp] = d + "ms";
+        return d;
+    }();
 
-          case 40: // down
-              evt.preventDefault();
-              viewport.move({x: viewport.x - 90});
-              break;
+    $(document).keydown(function (evt) {
+        switch (evt.keyCode) {
+            case 37: // left
+                viewport.move({
+                    y: viewport.y - 90
+                });
+                break;
 
-          case 27: //esc
-              viewport.reset();
-              break;
+            case 38: // up
+                evt.preventDefault();
+                viewport.move({
+                    x: viewport.x + 90
+                });
+                break;
 
-          default:
-              break;
-      };
-  }).bind('mousedown touchstart', function(evt) {
-      delete mouse.last;
-      if($(evt.target).is('a, iframe')) {
-          return true;
-      }
+            case 39: // right
+                viewport.move({
+                    y: viewport.y + 90
+                });
+                break;
 
-      evt.originalEvent.touches ? evt = evt.originalEvent.touches[0] : null;
-      mouse.start.x = evt.pageX;
-      mouse.start.y = evt.pageY;
-      $(document).bind('mousemove touchmove', function(event) {
-          // Only perform rotation if one touch or mouse (e.g. still scale with pinch and zoom)
-          if(!touch || !(event.originalEvent && event.originalEvent.touches.length > 1)) {
-              event.preventDefault();
-              // Get touch co-ords
-              event.originalEvent.touches ? event = event.originalEvent.touches[0] : null;
-              $('.viewport').trigger('move-viewport', {x: event.pageX, y: event.pageY});
-          }
-      });
+            case 40: // down
+                evt.preventDefault();
+                viewport.move({
+                    x: viewport.x - 90
+                });
+                break;
 
-      $(document).bind('mouseup touchend', function () {
-          $(document).unbind('mousemove touchmove');
-      });
-  });
+            case 27: //esc
+                viewport.reset();
+                break;
 
-  $('.viewport').bind('move-viewport', function(evt, movedMouse) {
+            default:
+                break;
+        };
+    }).bind('mousedown touchstart', function (evt) {
+        delete mouse.last;
+        if ($(evt.target).is('a, iframe')) {
+            return true;
+        }
 
-      // Reduce movement on touch screens
-      var movementScaleFactor = touch ? 4 : 1;
+        evt.originalEvent.touches ? evt = evt.originalEvent.touches[0] : null;
+        mouse.start.x = evt.pageX;
+        mouse.start.y = evt.pageY;
+        $(document).bind('mousemove touchmove', function (event) {
+            // Only perform rotation if one touch or mouse (e.g. still scale with pinch and zoom)
+            if (!touch || !(event.originalEvent && event.originalEvent.touches.length > 1)) {
+                event.preventDefault();
+                // Get touch co-ords
+                event.originalEvent.touches ? event = event.originalEvent.touches[0] : null;
+                $('.viewport').trigger('move-viewport', {
+                    x: event.pageX,
+                    y: event.pageY
+                });
+            }
+        });
 
-      if (!mouse.last) {
-          mouse.last = mouse.start;
-      } else {
-          if (forward(mouse.start.x, mouse.last.x) != forward(mouse.last.x, movedMouse.x)) {
-              mouse.start.x = mouse.last.x;
-          }
-          if (forward(mouse.start.y, mouse.last.y) != forward(mouse.last.y, movedMouse.y)) {
-              mouse.start.y = mouse.last.y;
-          }
-      }
-      let y = viewport.y - parseInt((mouse.start.x - movedMouse.x)/movementScaleFactor)
-      console.log(y)
+        $(document).bind('mouseup touchend', function () {
+            $(document).unbind('mousemove touchmove');
+        });
+    });
+
+    $('.viewport').bind('move-viewport', function (evt, movedMouse) {
+
+        // Reduce movement on touch screens
+        var movementScaleFactor = touch ? 4 : 2;
+
+        if (!mouse.last) {
+            mouse.last = mouse.start;
+        } else {
+            if (forward(mouse.start.x, mouse.last.x) != forward(mouse.last.x, movedMouse.x)) {
+                mouse.start.x = mouse.last.x;
+            }
+            if (forward(mouse.start.y, mouse.last.y) != forward(mouse.last.y, movedMouse.y)) {
+                mouse.start.y = mouse.last.y;
+            }
+        }
+
+        viewport.move({
+            x: viewport.x + parseInt((mouse.start.y - movedMouse.y) / movementScaleFactor),
+            y: viewport.y - parseInt((mouse.start.x - movedMouse.x) / movementScaleFactor)
+        });
+
+        mouse.last.x = movedMouse.x;
+        mouse.last.y = movedMouse.y;
+
+        function forward(v1, v2) {
+            return v1 >= v2 ? true : false;
+        }
+    });
 
 
-      viewport.move({
-          x: viewport.x + parseInt((mouse.start.y - movedMouse.y)/movementScaleFactor),
-          y: viewport.y - parseInt((mouse.start.x - movedMouse.x)/movementScaleFactor)
-      });
-
-      mouse.last.x = movedMouse.x;
-      mouse.last.y = movedMouse.y;
-
-      function forward(v1, v2) {
-          return v1 >= v2 ? true : false;
-      }
-  });
-
-  
 
 });
